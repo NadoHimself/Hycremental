78c1151056b245ac72b9b8ebac000181ea7ec8e1
Kielian <105607229+NadoHimself@users.noreply.github.com>
2026-02-03 18:13:19 +0100

    Fix ShopCommand to match Hytale API structure

--- src/main/java/de/ageofflair/hycremental/commands/ShopCommand.java
+++ src/main/java/de/ageofflair/hycremental/commands/ShopCommand.java
@@ -1,33 +1,37 @@
 package de.ageofflair.hycremental.commands;
 
-import com.hypixel.hytale.server.core.command.system.basecommands.CommandBase;
-import com.hypixel.hytale.server.core.command.context.CommandContext;
+import com.hypixel.hytale.server.core.command.Command;
+import com.hypixel.hytale.server.core.command.system.CommandSender;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.Message;
 import de.ageofflair.hycremental.Hycremental;
 
-import javax.annotation.Nonnull;
-
 /**
  * Shop Command - Open generator shop
  */
-public class ShopCommand extends CommandBase {
+public class ShopCommand extends Command {
     
     private final Hycremental plugin;
     
     public ShopCommand(Hycremental plugin) {
-        super("shop", "Open the generator shop");
         this.plugin = plugin;
     }
     
     @Override
-    protected void executeSync(@Nonnull CommandContext context) {
-        if (!context.isPlayer()) {
-            context.sendMessage(Message.raw("§cThis command can only be used by players!"));
+    public String getName() {
+        return "shop";
+    }
+    
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(Message.raw("§cThis command can only be used by players!"));
             return;
         }
         
-        context.sendMessage(Message.raw("§6§l=== Generator Shop ==="));
-        context.sendMessage(Message.raw("§7Shop UI coming soon!"));
+        Player player = (Player) sender;
+        
+        player.sendMessage(Message.raw("§6§l=== Generator Shop ==="));
+        player.sendMessage(Message.raw("§7Shop UI coming soon!"));
     }
 }
